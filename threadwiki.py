849f2ae8074992115967e1bbd993d3529b7c1a2b
artificialsoph <s@soph.info>
2019-08-08 09:48:58 -0700

    even smol-er

--- threadwiki.py
+++ threadwiki.py
@@ -1,8 +1,8 @@
-from path import Path;import mistune as m;from flask import Flask, request
-a,d=Flask(""),Path("p")
+import path;import mistune as m;import flask
+a,d=flask.Flask(""),path.Path("p")
 @a.route("/<p>")
 def s(p):
-	r,f=request.args,d/p;k=f.text()if f.exists()else""
+	r,f=flask.request.args,d/p;k=f.text()if f.exists()else""
 	if'n'in r: f.write_text(r['n'])
 	b=[f"<a href='/{l[2:]}'>{l[2:]}</a><br>"for l in d.files()if"/"+p in l.text()]
 	return f"<h1>{p}</h1>"+m.markdown(k)+"<form><textarea name=n></textarea><input type=submit>"+''.join(b)
