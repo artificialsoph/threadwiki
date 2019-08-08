55d89b89725d1bd6849f8f941d4f890aea92de6b
artificialsoph <s@soph.info>
2019-08-08 09:01:00 -0700

    shrink

--- threadwiki.py
+++ threadwiki.py
@@ -3,7 +3,6 @@ a,d=Flask(""),Path("p")
 @a.route("/<p>")
 def s(p):
 	r,f=request.args,d/p;k=f.text()if f.exists()else""
-	if'n'in r:
-		f.write_text(r['n'])
+	if'n'in r: f.write_text(r['n'])
 	b=[f"<a href='/{l[2:]}'>{l[2:]}</a><br>"for l in d.files()if"/"+p in l.text()]
 	return f"<h1>{p}</h1>"+m.markdown(k)+"<form><textarea name=n></textarea><input type=submit>"+''.join(b)
