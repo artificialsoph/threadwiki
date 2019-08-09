371dd635f07151978a6ac33d6096eeec66b3f448
artificialsoph <s@soph.info>
2019-08-08 17:24:25 -0700

    a little smaller

--- threadwiki.py
+++ threadwiki.py
@@ -1,7 +1,6 @@
-import path,flask
-import mistune as m
+import path,mistune,flask
 a,d=flask.Flask(""),path.Path("p")
 @a.route("/<p>")
 def s(p):
-	f=d/p;k=f.text();f.write_text(flask.request.args.get('n', k));b=[f"[{l[2:]}](/{l[2:]})\n"for l in d.files()if"/"+p in l.text()]
-	return m.markdown(f"#{p}\n{k}\n\n"+''.join(b))+f"<form><textarea name=n>{k}</textarea>\n<input type=submit>"
+	f=d/p;k=flask.request.args.get('n',f.text());f.write_text(k);b=[f"[{l[2:]}](/{l[2:]})\n"for l in d.files()if"/"+p in l.text()]
+	return mistune.markdown(f"#{p}\n{k}\n\n"+''.join(b))+f"<form><textarea name=n>{k}</textarea>\n<button>"
